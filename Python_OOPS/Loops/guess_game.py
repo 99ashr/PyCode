32376919e72be7405d3c450ca56351077963c711
Ash <gaurashish99@gmail.com>
2020-12-22 12:26:57 +0530

    making guess game

---
+++ Python_OOPS/Loops/guess_game.py
@@ -0,0 +1,10 @@
+import random
+
+
+def guess_the_number():
+    """This function is a game that gives a winner for every correct guess of a number chosen by the user!!!
+    """
+
+
+# winner = guess_the_number()
+
