ee65647f20e3263a49cc57c92207854cd2c40c7e
Ash <gaurashish99@gmail.com>
2020-12-23 22:30:14 +0530

    guess_game_unfinished_touch

--- Python_OOPS/Loops/guess_game.py
+++ Python_OOPS/Loops/guess_game.py
@@ -1,10 +1,24 @@
 import random
 
 
-def guess_the_number():
+def guess_the_number(to_be_guessed, guess):
     """This function is a game that gives a winner for every correct guess of a number chosen by the user!!!
     """
+    # print(number, " ", to_be_guessed)
+    while guess != to_be_guessed:
+        guess = int(input("Enter you guessed number here: "))
+        if guess < to_be_guessed:
+            print("Oops! your guess is far behind the number.\n Try Again! ")
+        elif guess > to_be_guessed:
+            print("Oh! no you've entered a greater number.\nPlease Try Again! ")
+        else:
+            print("Wow you have won the game!!!")
 
 
-# winner = guess_the_number()
+number = int(input("Enter the limit to your guess here: "))
+# number = 20
+to_be_guessed = int(number*random.random()) + \
+    1  # random number is generated here
+guess = 0
 
+winner = guess_the_number(to_be_guessed, guess)  # calling function
