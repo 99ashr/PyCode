5920cce26ba95bf409fc20519d64d9ee1ac03277
Ash <gaurashish99@gmail.com>
2020-12-27 00:31:31 +0530

    pythagoras_th.

---
+++ Python_OOPS/Loops/Pythagoras.py
@@ -0,0 +1,6 @@
+def pythagoras():
+    """This method is defined to calculate the pythagoras of the given 3 numbers"""
+    range_ = int(input(
+        "Enter the range upto which you want to find the combination of the pythagoras numbers: "))
+    for num1 in range(1, range_+1):
+        for num2 in range(num1, range_+1)
