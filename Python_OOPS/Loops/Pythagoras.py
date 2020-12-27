e26578cbe03c499ad1e99655bcf9d70d2df56f1b
Ash <gaurashish99@gmail.com>
2020-12-27 14:54:47 +0530

    pythagoras_sides_found

--- Python_OOPS/Loops/Pythagoras.py
+++ Python_OOPS/Loops/Pythagoras.py
@@ -1,6 +1,18 @@
 def pythagoras():
     """This method is defined to calculate the pythagoras of the given 3 numbers"""
+    pl = []
     range_ = int(input(
-        "Enter the range upto which you want to find the combination of the pythagoras numbers: "))
-    for num1 in range(1, range_+1):
-        for num2 in range(num1, range_+1)
+        "Enter the range up to which you want to find the combination of the pythagoras numbers: "))
+    for a in range(1, range_+1):
+        for b in range(a, range_+1):
+            for c in range(b, range_+1):
+                if a**2+b**2 == c**2:
+                    pl.append([a, b, c])
+                elif b**2+c**2 == a**2:
+                    pl.append([a, b, c])
+                elif c**2+a**2 == b**2:
+                    pl.append([a, b, c])
+    return pl
+
+
+print(pythagoras())
