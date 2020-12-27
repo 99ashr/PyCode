254c04c2de9a166fa41fc73e79bb18c7e8c705d1
Ash <gaurashish99@gmail.com>
2020-12-27 23:08:05 +0530

    patterns

---
+++ Python_OOPS/Patterns/Half_pyramid(right&left).py
@@ -0,0 +1,11 @@
+def right_half_pyramid(n):
+    for i in range(1, n+1):
+        for i in range(i):
+            print("*", end="")
+        print("\r")  # \r --> carriage return
+
+
+# right_half_pyramid(5)
+
+def left_half_pyramid(n):
+    for i in range()
