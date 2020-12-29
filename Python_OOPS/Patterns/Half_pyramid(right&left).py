1c6a506c3d4388e13563ca9b51ac648baf71846e
Ash <gaurashish99@gmail.com>
2020-12-29 10:41:55 +0530

    math_game_index

--- Python_OOPS/Patterns/Half_pyramid(right&left).py
+++ Python_OOPS/Patterns/Half_pyramid(right&left).py
@@ -7,5 +7,20 @@ def right_half_pyramid(n):
 
 # right_half_pyramid(5)
 
+
+def pypart(n):
+    myList = []
+    for i in range(1, n+1):
+        myList.append("*"*i)
+    print("\n".join(myList))
+
+
+# Driver Code
+n = 5
+pypart(n)
+
+
 def left_half_pyramid(n):
-    for i in range()
+    for i in range(1, n+1):
+        for i in range(1, (n+1)-i):
+            print(" ", end="")
