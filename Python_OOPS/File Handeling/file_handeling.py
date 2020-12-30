893de13ed14f9d4cb7d088693af489c6981ca474
Ash <gaurashish99@gmail.com>
2020-12-30 21:44:52 +0530

    user_input

--- Python_OOPS/File Handeling/file_handeling.py
+++ Python_OOPS/File Handeling/file_handeling.py
@@ -1,9 +1,26 @@
+def user_input():
+    """This method takes the filename by the user which will be used later in other methods
+    """
+    filename = input("Enter the name of the file here: ")
+    return filename
+
+
 def open_file(filename):
+    """This method is opening the file that has been passed by the user!!!
+
+    Args:
+        filename ([string]): [description]
+    """
     print("\t\tIn the open_file function")
     # file=open(filename,mode)
     file = open(filename, "r")
     print("\t\tFile is opened by this function!!!")
-    file.close()
+    return file
+
+
+def close_file(file):
+    """This function is called to close the file!!!
+    """
 
 
 def handel():
