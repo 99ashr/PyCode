ac450b1b7133d6092169195899940b8d29cc2587
99ashr <gaurashish99@gmail.com>
2021-01-10 12:10:20 +0530

    Playing_with_lambda

--- Python_OOPS/playing_with_lambda.py
+++ Python_OOPS/playing_with_lambda.py
@@ -1,2 +1,16 @@
 # def sq(a): return a*a
 # print(sq(3))
+
+# @ Filter function
+# & Syntax --> filter(function,iterable)
+
+# ----------------------------------- Algo ----------------------------------- #
+# ! required--> List of marks :mylist
+# ! i) Generate another list using list keyword
+# ! ii) filter every number from mylist and using the lambda function match the expression
+# ! Display the output
+# ---------------------------------------------------------------------------- #
+
+mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+new_list = list(filter(lambda x: x % 3 == 2, mylist))
+print(new_list)
