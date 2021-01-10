e92ab7e1098bc1fcfe64fe0f5d37f2cbb1a000b8
99ashr <gaurashish99@gmail.com>
2021-01-10 19:05:33 +0530

    playing_with_maps

--- Python_OOPS/playing_with_lambda.py
+++ Python_OOPS/playing_with_lambda.py
@@ -11,6 +11,14 @@
 # ! Display the output
 # ---------------------------------------------------------------------------- #
 
+# mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# new_list = list(filter(lambda x: x % 3 == 2, mylist))
+# print(new_list)
+
+
+# @ Map function
+# & Syntax --> map(function,iterable)
+
 mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-new_list = list(filter(lambda x: x % 3 == 2, mylist))
+new_list = list(map(lambda x: x % 3 == 2, mylist))
 print(new_list)
