0371ffc0dbcdbc532dd16f2c913bf58f2fbe72cd
99ashr <gaurashish99@gmail.com>
2021-01-12 22:36:16 +0530

    Number_Stream

---
+++ Python_OOPS/Generators/Number stream.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#* ------------------------------- Number Stream ------------------------------ #
+# ! Series numbers within the range and conditions
+b = (x for x in range(1, 100, -5))
+print(b)
+for y in b:
+    print(y)
+#* ------------------------------------ EOF ----------------------------------- #
