1c13de20a207ad1150d37d7263385271a589cf70
99ashr <gaurashish99@gmail.com>
2021-01-12 10:01:29 +0530

    map_function_within_filter_function

--- Python_OOPS/Requisites/filter with maps.py
+++ Python_OOPS/Requisites/filter with maps.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-#* ---------------------------- Filter within maps ---------------------------- #
+
+#* -------------------- Filter function within Map function ------------------- #
+
 x = list(map(lambda a: a+a, filter(lambda a: a >= 3, [1, 2, 3, 4])))
-print(x)
+print("Filter function within Map function", x)
+
+#* -------------------- Map function within Filter function ------------------- #
+y = list(filter(lambda a: a >= 3, map(lambda a: a+a, [1, 2, 3, 4])))
+print("Map function within filter function", y)
 #* ------------------------------------ EOF ----------------------------------- #
