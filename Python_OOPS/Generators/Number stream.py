ffce770d376db8825791cae43ca626d504ff36ff
99ashr <gaurashish99@gmail.com>
2021-01-13 22:55:44 +0530

    Sinewave_using_generator

--- Python_OOPS/Generators/Number stream.py
+++ Python_OOPS/Generators/Number stream.py
@@ -4,7 +4,6 @@
 # ! Series numbers within the range and conditions
 b = (x for x in range(100, 1, -5))
 print(b)
-# ? FIXME: for loop doesn't print
 for y in b:
     print(y)
 #* ------------------------------------ EOF ----------------------------------- #
