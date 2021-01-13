a02d815e0ac43b8f313175cfc7ea098cb5818c9b
99ashr <gaurashish99@gmail.com>
2021-01-13 13:40:21 +0530

    Error_Fixed

--- Python_OOPS/Generators/Number stream.py
+++ Python_OOPS/Generators/Number stream.py
@@ -2,7 +2,7 @@
 
 #* ------------------------------- Number Stream ------------------------------ #
 # ! Series numbers within the range and conditions
-b = (x for x in range(1, 100, -5))
+b = (x for x in range(100, 1, -5))
 print(b)
 # ? FIXME: for loop doesn't print
 for y in b:
