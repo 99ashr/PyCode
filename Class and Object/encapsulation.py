c852caafc57ad605905cfac86fc5a4042f7d6202
99ashr <gaurashish99@gmail.com>
2021-01-15 00:02:39 +0530

    encapsulation_example

---
+++ Class and Object/encapsulation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+#* ------------------------------- Encapsulation ------------------------------ #
+# ! Encapsulation can be achieved by declaring private methods/data members or by abstraction.
+
+# ---------------------------------------------------------------------------- #
+
+class Computer:
+
+    def __init__(self):
+        self.__maxprice = 900
+
+    def sell(self):
+        print("Selling Price: {}".format(self.__maxprice))
+
+    def setMaxPrice(self, price):
+        self.__maxprice = price
+
+
+c = Computer()
+c.sell()
+
+# change the price
+c.__maxprice = 1000
+c.sell()
+
+# using setter function
+c.setMaxPrice(1000)
+c.sell()
+# ---------------------------------------------------------------------------- #
+
+#* ------------------------------------ EOF ----------------------------------- #
