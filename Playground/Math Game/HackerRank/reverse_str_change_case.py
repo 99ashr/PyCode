fce52615901deb1c228d825b01be698419ae81d2
99ashr <gaurashish99@gmail.com>
2021-01-26 23:32:51 +0530

    reverse_string_swapcase

---
+++ Playground/Math Game/HackerRank/reverse_str_change_case.py
@@ -0,0 +1,19 @@
+def reverse_words_order_and_swap_cases(sentence):
+
+    # Write your code here
+    # s = sentence[::-1]
+    # print(s)
+    # print(sentence.swapcase())
+    sentence = sentence.swapcase()
+    s = sentence.split()
+    new_str = []
+    for i in s[::-1]:
+        new_str.append(i)
+    # print(new_str)
+    str1 = " "
+    return(str1.join(new_str))
+    # print(ns)
+
+
+if __name__ == '__main__':
+    reverse_words_order_and_swap_cases("aWESOME is cODING")
