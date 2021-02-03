3dc5799378219cb0dcfff66f3c75b2d6f112167b
99ashr <gaurashish99@gmail.com>
2021-02-03 15:04:33 +0530

    average_of_markes_upto_two_decimal_places

---
+++ Playground/HackerRank/avg_two_decimal_places.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#! ---------- Average of marks of given name upto two deciaml places ---------- #
+
+"""The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. Print the average of the marks array for the student name provided, showing 2 places after the decimal. 
+    """
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    avg = {}
+    for i, j in student_marks.items():
+        if i == query_name:
+            avg = (sum(j))/len(j)
+            print("{:.2f}".format(avg))
+
+#! ------------------------------------ EOF ----------------------------------- #
