b2c25e59289fe97420e3cbcf0a5dfb46455b831a
99ashr <gaurashish99@gmail.com>
2021-02-06 23:16:56 +0530

    input_method_for_all

--- Playground/HackerRank/built_in_ins.py
+++ Playground/HackerRank/built_in_ins.py
@@ -4,3 +4,15 @@
 n = int(input().strip())
 arr = map(int, input().strip().split(' '))
 print(sum(arr))
+
+# ---------------------------------------------------------------------------- #
+
+N = input()
+M = map(int, input().split())
+if all(k > 0 for k in M):
+    if any(str(i)[::-1] == str(i) for i in M):
+        print(True)
+    else:
+        print(False)
+else:
+    print(False)
