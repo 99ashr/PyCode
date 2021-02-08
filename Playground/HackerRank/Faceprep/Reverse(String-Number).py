95a037a9c3d3ef97984f7d5332c477815ad5c30a
99ashr <gaurashish99@gmail.com>
2021-02-08 22:47:59 +0530

    reverse_a_number_and_a_string

--- Playground/HackerRank/Faceprep/Reverse(String-Number).py
+++ Playground/HackerRank/Faceprep/Reverse(String-Number).py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+num = 123456
+string = 'this is a string'
+
+
+# * Convert int into str object then follow the same procedure as you do for string
+
+str_num = str(num)
+print(str_num[::-1])
+
+
+str_list = string.split()
+
+print("String Reversed-->", string[::-1])
+print("Word Reversed-->", [i for i in str_list[::-1]])
