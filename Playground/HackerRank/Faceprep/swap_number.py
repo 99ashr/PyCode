b6359e2aa846f4b8a7ecba1c22b1451e3255ceb0
99ashr <gaurashish99@gmail.com>
2021-02-11 15:47:19 +0530

    Swap_number

---
+++ Playground/HackerRank/Faceprep/swap_number.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+#! ------------------------------- swap_number ------------------------------- #
+# Start your code from here!
+def swap_number(a, b):
+    a, b = b, a
+    return (a, b)
+
+
+print(swap_number(10, 100))
+
+#! ------------------------------------ EOF ----------------------------------- #
