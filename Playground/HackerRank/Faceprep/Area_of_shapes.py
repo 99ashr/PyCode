beeac8bc3a857e5f06c9739ea5dfd2ca93ef72fd
99ashr <gaurashish99@gmail.com>
2021-02-11 15:38:44 +0530

    Area_of_shapes

---
+++ Playground/HackerRank/Faceprep/Area_of_shapes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+#! ------------------------------- Area_of_shapes ------------------------------- #
+# Start your code from here!
+def rect(l, b):
+    area = l*b
+    return area
+
+
+def square(s):
+    area = s * s
+    return area
+
+
+def circle(r):
+    area = (1.41 * (r ** 2))
+    return area
+
+
+print(rect(9, 4))
+print(square(5))
+print(circle(8))
+#! ------------------------------------ EOF ----------------------------------- #
